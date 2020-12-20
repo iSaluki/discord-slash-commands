c0690f3fcea3245bf7323db027c3c34a6a4101e8
iSaluki <37915796+iSaluki@users.noreply.github.com>
2020-12-20 12:03:08 +0000

    Create discord-slash-commands.py
    Working script

---
+++ discord-slash-commands.py
@@ -0,0 +1,67 @@
+import requests
+#import numpy
+
+application_id = input("Enter application ID: ")
+while not application_id.isdigit():
+    application_id = input("Enter application ID: ")
+token = input("Enter bot token: ")
+cmdType = input("Global or Guild?: ")
+if cmdType.lower() == "global":
+    url = "https://discord.com/api/v8/applications/"+application_id+"/commands"
+elif cmdType.lower() == "guild":
+    guild_id = input("Enter guild ID: ")
+    while not guild_id.isdigit():
+        guild_id = input("Enter guild ID: ")
+    url = "https://discord.com/api/v8/applications/"+application_id+"/guilds/"+guild_id+"/commands"
+else:
+    print ("Please choose a valid option.")
+
+cmd_name = input("Enter command name: ")
+cmd_description = input("Enter command description: ")
+value_name = input ("Value name: ")
+value_description = input("Value description: ")
+#cmd_Type = input("Enter command type (integer): ")
+cmd_isRequired = input("Is the command required? (True/False): ")
+cmd_isRequired = bool(cmd_isRequired)
+#while not cmd_Type.isdigit():
+#    cmd_Type = input("Enter command type (integer): ")
+#cmd_Type = int(cmd_Type)
+
+print ("Starting choices editor...")
+
+#choices = []
+#stop = "n"
+#while stop == "n":
+#    choiceName = input("Enter choice name: ")
+#    choiceValue = input("Enter choice value: ")
+#    choices.append([choiceName, choiceValue])
+#    stop = input("Done? (y/n): ")[0].lower()
+
+
+#choicesNum = len(choices)
+#for choice in choices:
+    
+
+json = {
+    "name": cmd_name,
+    "description": cmd_description,
+    "options": [
+        {
+            "name": value_name,
+            "description": value_description,
+            "type": 3,
+            "required": cmd_isRequired,
+
+        },
+    ]
+}
+
+headers = {
+    "Authorization":"Bot "+ token 
+}
+
+print(json)
+print(headers)
+
+r = requests.post(url, headers=headers, json=json)
+print(r.content)
